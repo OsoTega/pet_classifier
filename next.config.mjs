8d703ec7b999262e93bf199c02ed193d4dc44bb2
OsoTega <96957781+OsoTega@users.noreply.github.com>
2024-05-25 19:02:07 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ['@tensorflow/tfjs-node'],
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                fs: false,
-                path: false,
-                os: false,
-                crypto: false,
-                stream: false,
-                http: false,
-                https: false,
-                buffer: false,
-                util: false,
-                url: false,
-                assert: false,
-                // Add other Node.js modules as needed
-            };
-        }
-
-        return config;
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
